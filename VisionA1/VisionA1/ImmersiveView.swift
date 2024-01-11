0648815f30275b4575b32571c32c4db9dbd6a6c9
Joel Sereno <jakayus123@gmail.com>
2024-01-10 21:45:38 -0700

    add physic properties and flooring

--- VisionA1/VisionA1/ImmersiveView.swift
+++ VisionA1/VisionA1/ImmersiveView.swift
@@ -15,11 +15,40 @@ struct ImmersiveView: View {
             
             // add 3D scene
             
-            // create an entity and entity model
+            /// Create a floor
+            
+            // create floor
+            let floor = ModelEntity(mesh: .generatePlane(width: 50, depth: 50), materials: [OcclusionMaterial()])
+            // create physics properties
+            floor.generateCollisionShapes(recursive: false)
+            floor.components[PhysicsBodyComponent.self] = .init(
+                massProperties: .default,
+                mode: .static // does not move
+            )
+            // add floor to content
+            content.add(floor)
+            
+            
+            /// create an entity and entity model
+        
             // model for USDZ type files include two children
             if let boxModel = try? await Entity(named: "box"), let box = boxModel.children.first?.children.first {
+                // positioning
                 box.position.y = 0.5
                 box.position.z = -1
+                
+                // physics properties
+                
+                box.generateCollisionShapes(recursive: false)
+                box.components.set(InputTargetComponent())
+                
+                box.components[PhysicsBodyComponent.self] = .init(
+                    massProperties: .default,
+                    material: .generate(staticFriction: 0.8, dynamicFriction: 0.5, restitution: 0.1), // restituation is bounciness
+                    mode: .dynamic
+                )
+                box.components[PhysicsMotionComponent.self] = .init()
+                
                 content.add(box)
             }
             
